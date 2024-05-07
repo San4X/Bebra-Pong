37f209d7a0eb706477bd781f6aae40297906dfe9
San4X <totalzroz@gmail.com>
2024-05-07 14:27:23 +0300

    different bounciness from player

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Reflection.Emit;
-using Unity.VisualScripting;
 using UnityEngine;
 using Random = System.Random;
 
@@ -16,8 +14,6 @@ public class Ball : MonoBehaviour
     private int _ranY;
     private Vector2 _ranDir;
     private Vector2 _tempDir;
-
-    private Vector2 _reflectedVector = new Vector2((float)0.71,(float)-0.71);
     private Vector2 _inDirection = new Vector2(0, 0);
     
     // Start is called before the first frame update
@@ -45,9 +41,61 @@ public class Ball : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision) //_inDirection initialized at the beginning because after ball collides with another object it instantly changes its trajectory and only after Vector2.Reflect compilates so it need unchangeble variable of Vector2.
     {
         Vector2 inNormal = collision.contacts[0].normal;
-        Vector2 newVelocityDir = Vector2.Reflect(_inDirection , inNormal);
-        Debug.Log($"Old direction = {_inDirection} to new direction {newVelocityDir}");
-        rb.velocity = newVelocityDir;
+        Vector2 reflectedVelocityDir = Vector2.Reflect(_inDirection , inNormal);
+
+        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            _inDirection = reflectedVelocityDir;
+
+            AdjustTrajectory(45, collision);
+        }
+        
+        Debug.Log($"Old direction = {_inDirection} to new direction {reflectedVelocityDir}");
+        //rb.velocity = newVelocityDir;
+        
+        rb.velocity = _inDirection;
         _inDirection = rb.velocity;
     }
+    
+    void AdjustTrajectory(float angle, Collision2D collision)
+    {
+        ContactPoint2D contact = collision.contacts[0]; // Get the first contact point
+        float top = collision.collider.bounds.max.y;
+        float bot = collision.collider.bounds.min.y;
+        float angleMultiplier;
+
+        if (_inDirection.y < 0)
+        {
+            angleMultiplier = Mathf.InverseLerp( top, bot, contact.point.y);
+        }
+        else
+        {
+            angleMultiplier = Mathf.InverseLerp( bot, top, contact.point.y);
+            angle-= angle*2;
+        }
+        
+        // Convert the angle from degrees to radians
+        float radians = (angle * angleMultiplier) * Mathf.Deg2Rad;
+
+        // Calculate the sine and cosine of the angle
+        float cosAngle = Mathf.Cos(radians);
+        float sinAngle = Mathf.Sin(radians);
+
+        // Perform a 2D rotation of the velocity vector by the specified angle
+        float newX = _inDirection.x * cosAngle - _inDirection.y * sinAngle;
+        float newY = _inDirection.x * sinAngle + _inDirection.y * cosAngle;
+
+        // Update the velocity vector with the adjusted trajectory
+        _inDirection.x = newX;
+        _inDirection.y = newY;
+
+        // Normalize the adjusted velocity vector to maintain its direction
+        _inDirection.Normalize();
+    }
 }
+
+
+// Визначаємо координати точки дотику
+// конвертуємо її в число від 0 до 1 де 0 це сторона з якої прилітає м'яч
+// чим більше число тим менший кут відбиття і навпаки
