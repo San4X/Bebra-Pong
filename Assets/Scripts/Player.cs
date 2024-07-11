28298f778544efd920747bdc4a1ee3a18110ea38
San4X <totalzroz@gmail.com>
2024-07-11 13:12:02 +0300

    Player script refactor
    Player do not slide through walls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,42 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public GameObject player;
-    public GameObject top;
-    public GameObject bottom;
+    public GameObject upperFrame;
+    public GameObject lowerFrame;
 
     public float speed = 2f;
-
-    private float playerHeight = 0;
-
-    private float maxY;
-    private float minY;
-    private float currentX;
-
-    private Rigidbody2D rb;
+    
+    private float _upperBorder;
+    private float _lowerBorder;
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        BoxCollider2D boxCollider = GetComponent<BoxCollider2D>();
-        if (boxCollider != null)
-        {
-            Vector2 size = boxCollider.size;
-            playerHeight = size.y;
-        }
-
-        maxY = top.transform.position.y + playerHeight / -2f;
-        minY = bottom.transform.position.y + playerHeight / 2f;
-        currentX = player.transform.position.x;
+        float playerSizeY = GetComponent<BoxCollider2D>().size.y;
+        float upperFrameSizeY = upperFrame.GetComponent<BoxCollider2D>().size.y;
+        float lowerFrameSizeY = lowerFrame.GetComponent<BoxCollider2D>().size.y;
+        
+        _upperBorder = upperFrame.transform.position.y - upperFrameSizeY / 2f - playerSizeY / 3f;
+        _lowerBorder = lowerFrame.transform.position.y + lowerFrameSizeY / 2f + playerSizeY / 3f;
     }
     
     // Update is called once per frame
     void Update()
     {
         float verticalInput = Input.GetAxis("Vertical");
-        player.transform.Translate(new Vector3(0, verticalInput, 0) * (speed * Time.deltaTime));
-        float currentY = player.transform.position.y;
-        player.transform.position = new Vector3(currentX,Mathf.Clamp(currentY, minY, maxY), 0);
+        transform.Translate(new Vector3(0, verticalInput, 0) * (speed * Time.deltaTime));
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, _lowerBorder, _upperBorder), 0);
     }
 }
