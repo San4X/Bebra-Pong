79bc2d71149fa3814844dc67621ff6e1b1eac818
San4X <totalzroz@gmail.com>
2024-11-02 22:57:39 +0200

    add namespace

--- Assets/Scripts/Network/SpawnManager.cs
+++ Assets/Scripts/Network/SpawnManager.cs
@@ -1,62 +1,63 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SpawnManager : NetworkBehaviour
+namespace Network
 {
-    public static SpawnManager Instance { get; private set; }
+    public class SpawnManager : NetworkBehaviour
+    {
+        public static SpawnManager Instance { get; private set; }
 
-    [SerializeField] private Transform playerPrefab, ballPrefab;
-    [SerializeField] private GameObject aiPrefab;
-    [SerializeField] private Transform player1SpawnPoint;
-    [SerializeField] private Transform player2SpawnPoint;
+        [SerializeField] private Transform playerPrefab, ballPrefab;
+        [SerializeField] private GameObject aiPrefab;
+        [SerializeField] private Transform player1SpawnPoint;
+        [SerializeField] private Transform player2SpawnPoint;
     
 
-    private void Awake()
-    {
-        Instance = this;
-        DontDestroyOnLoad(gameObject);
-    }
-
-    private void Start()
-    {
-        if (GameModeManager.IsSolo == true) SetupAIScene();
-        if (NetworkManager.Singleton.IsServer)
+        private void Awake()
         {
-            NetworkManager.Singleton.SceneManager.OnLoadEventCompleted += SceneManager_OnLoadEventCompleted;
-        }
-    }
-
-    private void SceneManager_OnLoadEventCompleted(string scenename, LoadSceneMode loadscenemode, List<ulong> clientscompleted, List<ulong> clientstimedout)
-    {
-        foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
-        {
-            bool isServer = clientId == NetworkManager.Singleton.LocalClientId;
-            
-            Transform playerTransform = isServer
-                ? Instantiate(playerPrefab, player1SpawnPoint.position, playerPrefab.rotation)
-                : Instantiate(playerPrefab, player2SpawnPoint.position, playerPrefab.rotation);
-            
-            NetworkObject networkObject = playerTransform.GetComponent<NetworkObject>();
-            networkObject.SpawnAsPlayerObject(clientId, true);
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
         }
 
-        Transform ballTransform = Instantiate(ballPrefab);
-        NetworkObject ballNetworkObject = ballTransform.GetComponent<NetworkObject>();
-        ballNetworkObject.Spawn(true);
-    }
+        private void Start()
+        {
+            if (GameModeManager.IsSolo == true) SetupAIScene();
+            if (NetworkManager.Singleton.IsServer)
+            {
+                NetworkManager.Singleton.SceneManager.OnLoadEventCompleted += SceneManager_OnLoadEventCompleted;
+            }
+        }
+
+        private void SceneManager_OnLoadEventCompleted(string scenename, LoadSceneMode loadscenemode, List<ulong> clientscompleted, List<ulong> clientstimedout)
+        {
+            foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
+            {
+                bool isServer = clientId == NetworkManager.Singleton.LocalClientId;
+            
+                Transform playerTransform = isServer
+                    ? Instantiate(playerPrefab, player1SpawnPoint.position, playerPrefab.rotation)
+                    : Instantiate(playerPrefab, player2SpawnPoint.position, playerPrefab.rotation);
+            
+                NetworkObject networkObject = playerTransform.GetComponent<NetworkObject>();
+                networkObject.SpawnAsPlayerObject(clientId, true);
+            }
+
+            Transform ballTransform = Instantiate(ballPrefab);
+            NetworkObject ballNetworkObject = ballTransform.GetComponent<NetworkObject>();
+            ballNetworkObject.Spawn(true);
+        }
     
-    private void SetupAIScene()
-    {
-        Instantiate(playerPrefab, new Vector3(-8f, 0, 0), playerPrefab.transform.rotation);
-        Instantiate(aiPrefab, new Vector3(8f, 0, 0), aiPrefab.transform.rotation);
-    }
+        private void SetupAIScene()
+        {
+            Instantiate(playerPrefab, new Vector3(-8f, 0, 0), playerPrefab.transform.rotation);
+            Instantiate(aiPrefab, new Vector3(8f, 0, 0), aiPrefab.transform.rotation);
+        }
     
-    public void LoadMenuScene()
-    {
-        SceneManager.LoadScene("Menu");
+        public void LoadMenuScene()
+        {
+            SceneManager.LoadScene("Menu");
+        }
     }
 }
